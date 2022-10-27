558759b0fcbda78fa8659c7a704d9f5fc3ba955e
Leonel Chauca <leonelchauca0@gmail.com>
2022-10-26 21:21:36 -0400

    primer commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title:string = 'Contador App';
+  numero:number=0;
+
+  base:number = 3;
+
+  acumular(valor:number){
+    this.numero +=valor;
+  }
+
+
+}
